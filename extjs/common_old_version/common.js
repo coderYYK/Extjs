a5d1b53c21bf9fb555b5fce11517ccbce6c1ad2f
HP-OMEN-YYK <2459121238@qq.com>
2024-07-15 15:23:15 +0800

    add

--- extjs/common_old_version/common.js
+++ extjs/common_old_version/common.js
@@ -1,184 +1,198 @@
+/** @format */
+
 var MyCim = MyCim || {
-  version:"1.0"
-};
-MyCim.notify = function(){
-    var msgCt;
-    function createBox(t, s){
-       return '<div class="msg"><h3>' + t + '</h3><p>' + s + '</p></div>';
-    }
-    return {
-        msg : function(title, format){
-            if(!msgCt){
-                msgCt = Ext.DomHelper.insertFirst(document.body, {id:'msg-div'}, true);
-            }
-            var s = Ext.String.format.apply(String, Array.prototype.slice.call(arguments, 1));
-            var m = Ext.DomHelper.append(msgCt, createBox(title, s), true);
-            m.hide();
-            m.slideIn('b').ghost("b", { delay: 2000, remove: true});
-        },
-        showWaiting: function() {
-            Ext.MessageBox.show({
-            msg: '数据处理中，请稍后....',
-            progressText: '处理中...',
-            width:300,
-            wait:true,
-            waitConfig: {interval:100},
-            icon:'ext-mb-loading'
-          });
-        },
-        hideWaiting: function() {
-          Ext.MessageBox.hide();
-        },
-        alert: function(msg, fn) {
-          Ext.MessageBox.alert("警告", msg, fn);
-        },
-        confirm: function(msg, fn) {
-          Ext.MessageBox.confirm("确认", msg, fn);
-        },
-        alertParent: function(msg, fn) {
-          window.parent.frames.MyCim.notify.alert(msg, fn);
-        },
-        showParentWaiting: function(msg, fn) {
-          window.parent.frames.MyCim.notify.showWaiting();
-        },
-        hideParentWaiting: function(msg, fn) {
-          window.parent.frames.MyCim.notify.hideWaiting();
-        },
-        init : function(){}
-    };
-}();
-Ext.mask = function() {
-  return {
-    showBodyMask: function() {
-      if(Ext.getBody()!=null) {Ext.getBody().mask();}
-    },
-    hideBodyMask: function() {
-      if(Ext.getBody().isMasked()) {
-        Ext.getBody().unmask();
-      }
-    }
-  };
-}();
-Ext.storage = function(){
-  var storage;
-  function checkStorage() {// check storage
-    if (!window.localStorage) {
-      alert('This browser does NOT support localStorage,/n'
-        + 'Please update your browser to IE8+ or Chrome.');
-    } else {
-      if(!storage) {storage = window.localStorage;}     
-    }
-    return !!window.localStorage;
+  version: "1.0"
+}
+MyCim.notify = (function () {
+  var msgCt
+  function createBox(t, s) {
+    return '<div class="msg"><h3>' + t + "</h3><p>" + s + "</p></div>"
   }
   return {
-    put: function(key,value) {
-      if(checkStorage()) {storage.setItem(key,value);}
+    msg: function (title, format) {
+      if (!msgCt) {
+        msgCt = Ext.DomHelper.insertFirst(document.body, { id: "msg-div" }, true)
+      }
+      var s = Ext.String.format.apply(String, Array.prototype.slice.call(arguments, 1))
+      var m = Ext.DomHelper.append(msgCt, createBox(title, s), true)
+      m.hide()
+      m.slideIn("b").ghost("b", { delay: 2000, remove: true })
     },
-    remove: function(key) {
-      if(checkStorage()) {storage.removeItem(key);}
+    showWaiting: function () {
+      Ext.MessageBox.show({
+        msg: "数据处理中，请稍后....",
+        progressText: "处理中...",
+        width: 300,
+        wait: true,
+        waitConfig: { interval: 100 },
+        icon: "ext-mb-loading"
+      })
     },
-    get: function(key) {
-      if(checkStorage()) {return storage.getItem(key);}
+    hideWaiting: function () {
+      Ext.MessageBox.hide()
     },
-    getAll:function() {
-      if(checkStorage()) {
-        for(var i=0;i<storage.length;i++){
-           alert(storage.key(i)+ " : " + storage.getItem(storage.key(i)));
+    alert: function (msg, fn) {
+      Ext.MessageBox.alert("警告", msg, fn)
+    },
+    confirm: function (msg, fn) {
+      Ext.MessageBox.confirm("确认", msg, fn)
+    },
+    alertParent: function (msg, fn) {
+      window.parent.frames.MyCim.notify.alert(msg, fn)
+    },
+    showParentWaiting: function (msg, fn) {
+      window.parent.frames.MyCim.notify.showWaiting()
+    },
+    hideParentWaiting: function (msg, fn) {
+      window.parent.frames.MyCim.notify.hideWaiting()
+    },
+    init: function () {}
+  }
+})()
+Ext.mask = (function () {
+  return {
+    showBodyMask: function () {
+      if (Ext.getBody() != null) {
+        Ext.getBody().mask()
+      }
+    },
+    hideBodyMask: function () {
+      if (Ext.getBody().isMasked()) {
+        Ext.getBody().unmask()
+      }
+    }
+  }
+})()
+Ext.storage = (function () {
+  var storage
+  function checkStorage() {
+    // check storage
+    if (!window.localStorage) {
+      alert("This browser does NOT support localStorage,/n" + "Please update your browser to IE8+ or Chrome.")
+    } else {
+      if (!storage) {
+        storage = window.localStorage
+      }
+    }
+    return !!window.localStorage
+  }
+  return {
+    put: function (key, value) {
+      if (checkStorage()) {
+        storage.setItem(key, value)
+      }
+    },
+    remove: function (key) {
+      if (checkStorage()) {
+        storage.removeItem(key)
+      }
+    },
+    get: function (key) {
+      if (checkStorage()) {
+        return storage.getItem(key)
+      }
+    },
+    getAll: function () {
+      if (checkStorage()) {
+        for (var i = 0; i < storage.length; i++) {
+          alert(storage.key(i) + " : " + storage.getItem(storage.key(i)))
         }
       }
     },
-    clear: function() {
-      if(checkStorage()) {storage.clear();}
+    clear: function () {
+      if (checkStorage()) {
+        storage.clear()
+      }
     }
-  };
-}();
-Ext.themeName = 'classic';
-var required = '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>';
-Ext.BLANK_IMAGE_URL='/mycim2/app/common/js/extjs/resources/themes/images/default/tree/s.gif';
-Ext.Ajax.timeout = 300000; // 5 minutes
+  }
+})()
+Ext.themeName = "classic"
+var required = '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>'
+Ext.BLANK_IMAGE_URL = "/mycim2/app/common/js/extjs/resources/themes/images/default/tree/s.gif"
+Ext.Ajax.timeout = 300000 // 5 minutes
 Ext.data.proxy.Ajax.override({
-    actionMethods: {
-        create: 'POST',
-        read: 'POST',
-        update: 'POST',
-        destroy: 'POST'
-    }
-});
-Ext.Ajax.on('beforerequest',function(conn,options,eOpts) {
-  MyCim.notify.showWaiting();
-});
-Ext.Ajax.on('requestcomplete',function(conn,resp,options) {
-  MyCim.notify.hideWaiting();
-  var sessionOut = resp.responseText.match('relogin.jsp');
-  if (sessionOut!=null) {
-    window.location.href='/mycim2/relogin.jsp';
-    return false;
+  actionMethods: {
+    create: "POST",
+    read: "POST",
+    update: "POST",
+    destroy: "POST"
   }
-  
-  var responseJson = null;
+})
+Ext.Ajax.on("beforerequest", function (conn, options, eOpts) {
+  MyCim.notify.showWaiting()
+})
+Ext.Ajax.on("requestcomplete", function (conn, resp, options) {
+  MyCim.notify.hideWaiting()
+  var sessionOut = resp.responseText.match("relogin.jsp")
+  if (sessionOut != null) {
+    window.location.href = "/mycim2/relogin.jsp"
+    return false
+  }
+
+  var responseJson = null
   try {
-    responseJson = Ext.JSON.decode(resp.responseText);
-  } catch(e){
+    responseJson = Ext.JSON.decode(resp.responseText)
+  } catch (e) {
     //MyCim.notify.alert('服务器端异常或等待超时,请稍后重试!');
-    return false;
+    return false
   }
-  
+
   // The store data
-  var operation = options.operation;
-  var successCallback = null;
-  var errorCallback = null;
-  if(operation!=null) {
-    successCallback =  Ext.isFunction(operation.doSuccessCallback) ? operation.doSuccessCallback : Ext.emptyFn;
-    errorCallback = Ext.isFunction(operation.doErrorCallback) ? operation.doErrorCallback : Ext.emptyFn;
+  var operation = options.operation
+  var successCallback = null
+  var errorCallback = null
+  if (operation != null) {
+    successCallback = Ext.isFunction(operation.doSuccessCallback) ? operation.doSuccessCallback : Ext.emptyFn
+    errorCallback = Ext.isFunction(operation.doErrorCallback) ? operation.doErrorCallback : Ext.emptyFn
     if (responseJson.success != null && !responseJson.success) {
-      MyCim.notify.alert(responseJson.msg,errorCallback(responseJson.msg,operation,false));
-      return false;
+      MyCim.notify.alert(responseJson.msg, errorCallback(responseJson.msg, operation, false))
+      return false
     } else {
-      Ext.callback(successCallback(responseJson,operation,true),this);
-      return true;
+      Ext.callback(successCallback(responseJson, operation, true), this)
+      return true
     }
   }
-  return true;
-});
-Ext.Ajax.on('requestexception',function(conn,resp,options) {
-  MyCim.notify.hideWaiting();
-  MyCim.notify.alert('服务器端异常或等待超时,请稍后重试!');
-});
-Ext.override(Ext.form.field.Base,{
-  initComponent:function(){ 
-    if(this.allowBlank!==undefined && !this.allowBlank){
-      this.afterLabelTextTpl = '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>';
+  return true
+})
+Ext.Ajax.on("requestexception", function (conn, resp, options) {
+  MyCim.notify.hideWaiting()
+  MyCim.notify.alert("服务器端异常或等待超时,请稍后重试!")
+})
+Ext.override(Ext.form.field.Base, {
+  initComponent: function () {
+    if (this.allowBlank !== undefined && !this.allowBlank) {
+      this.afterLabelTextTpl = '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>'
     }
-    this.callParent(arguments); 　　
-    } 
-});
+    this.callParent(arguments)
+  }
+})
 
 /**
  * Sets the read only state of this field.
  */
-Ext.override(Ext.form.field.Base,{
-  setReadOnly: function(readOnly) {
+Ext.override(Ext.form.field.Base, {
+  setReadOnly: function (readOnly) {
     var me = this,
-        inputEl = me.inputEl;
-    readOnly = !!readOnly;
-    me.readOnly = readOnly;
+      inputEl = me.inputEl
+    readOnly = !!readOnly
+    me.readOnly = readOnly
     if (inputEl) {
-        inputEl.dom.readOnly = readOnly;
-        if(readOnly) {
-          inputEl.addCls('x-form-readonly');          
-        } else {
-          inputEl.removeCls('x-form-readonly');
-        }
+      inputEl.dom.readOnly = readOnly
+      if (readOnly) {
+        inputEl.addCls("x-form-readonly")
+      } else {
+        inputEl.removeCls("x-form-readonly")
+      }
     } else if (me.rendering) {
-        me.setReadOnlyOnBoxReady = true;
+      me.setReadOnlyOnBoxReady = true
     }
-    me.fireEvent('writeablechange', me, readOnly);
-}
-});
+    me.fireEvent("writeablechange", me, readOnly)
+  }
+})
 /**
  * Enable text selectionF
  */
-Ext.override(Ext.grid.View, { enableTextSelection: true });
+Ext.override(Ext.grid.View, { enableTextSelection: true })
 //每一个列都会出现鼠标悬浮上去显示内容
 /**
  * //适用于Extjs4.x
@@ -188,31 +202,30 @@ Ext.override(Ext.grid.View, { enableTextSelection: true });
  * 单元格数据显示不完整 ,增加title 浮动提示信息
  */
 Ext.override(Ext.grid.GridPanel, {
-    afterRender : Ext.Function.createSequence(Ext.grid.GridPanel.prototype.afterRender,
-        function() {
-            /* 默认显示提示
+  afterRender: Ext.Function.createSequence(Ext.grid.GridPanel.prototype.afterRender, function () {
+    /* 默认显示提示
             if (!this.cellTip) {
                 return;
             }*/
-            var view = this.getView();
-            this.tip = new Ext.ToolTip({
-                target: view.el,
-                delegate : '.x-grid-cell-inner',
-                trackMouse: true,
-                renderTo: Ext.getBody(),
-                listeners: {
-                    beforeshow: function updateTipBody(tip) {
-                        //取cell的值
-                        //fireFox  tip.triggerElement.textContent
-                        //IE  tip.triggerElement.innerText
-                        var tipText = (tip.triggerElement.innerText || tip.triggerElement.textContent);
-                        if (Ext.isEmpty(tipText) || Ext.isEmpty(tipText.trim()) ) {
-                            return false;
-                        }
+    var view = this.getView()
+    this.tip = new Ext.ToolTip({
+      target: view.el,
+      delegate: ".x-grid-cell-inner",
+      trackMouse: true,
+      renderTo: Ext.getBody(),
+      listeners: {
+        beforeshow: function updateTipBody(tip) {
+          //取cell的值
+          //fireFox  tip.triggerElement.textContent
+          //IE  tip.triggerElement.innerText
+          var tipText = tip.triggerElement.innerText || tip.triggerElement.textContent
+          if (Ext.isEmpty(tipText) || Ext.isEmpty(tipText.trim())) {
+            return false
+          }
 
-                        tip.update(tipText);
-                    }
-                }
-            });
-        })
-});
+          tip.update(tipText)
+        }
+      }
+    })
+  })
+})
--- pages/jjCompeteDetail/js/define.js
+++ pages/jjCompeteDetail/js/define.js
@@ -1,114 +1,159 @@
+/** @format */
+const PAGE_SIZE = 20
 function createQryForm() {
-	const qryForm = Ext.create('InitViewer.QryForm', {
-		id: 'qryForm',
-		title: '机加完工明细',
-		region: 'north'
-	});
-	const form = qryForm.getForm();
+  const qryForm = Ext.create("InitViewer.QryForm", {
+    id: "qryForm",
+    title: "机加完工明细",
+    region: "north"
+  })
+  const form = qryForm.getForm()
 
-	const qryBtn = qryForm.query('[name="qryBtn"]')[0];
-	qryBtn.on({
-		click: () => {
-			const id = form.findField('squareStickId').getValue();
-			if (!id) {
-				return;
-			}
-			getJjBasicInfo(id, (data) => {
-				const basicForm = Ext.getCmp('basicInfoForm').getForm();
-				basicForm.findField('squareStickId').setValue(data.squareStickId);
-				basicForm.findField('workorderId').setValue(data.workorderId);
-				basicForm.findField('jjTechnologyWorkflowId').setValue(data.jjTechnologyWorkflowId);
-				basicForm.findField('technologyWorkflowVersion').setValue(data.technologyWorkflowVersion);
-				basicForm.findField('productSpecBook').setValue(data.productSpecBook);
-				basicForm.findField('squareStickStatus').setValue(data.squareStickStatus);
-				basicForm.findField('hairStickId').setValue(data.hairStickId);
-				basicForm.findField('hairStickFurnaceId').setValue(data.hairStickFurnaceId);
-				basicForm.findField('furnaceTaskId').setValue(data.furnaceTaskId);
-			});
-		}
-	});
-	return qryForm;
+  const qryBtn = qryForm.query('[name="qryBtn"]')[0]
+  qryBtn.on({
+    click: () => {
+      const id = form.findField("squareStickId").getValue()
+      if (!id) {
+        return
+      }
+      const activeTab = Ext.getCmp("detailTabPanel").getActiveTab()
+      if (activeTab.id === "detailGrid") {
+        const detailStore = Ext.data.StoreManager.lookup("detailStore")
+        detailStoreReload(detailStore)
+      } else if (activeTab.id === "basicInfoForm") {
+        getJjBasicInfo(id, (data) => {
+          const basicForm = Ext.getCmp("basicInfoForm").getForm()
+          setBasicFormInfo(basicForm, data)
+        })
+      }
+    }
+  })
+  return qryForm
 }
 
 function createTabPanel() {
-	const tabPanel = Ext.create('Ext.tab.Panel', {
-		region: 'center',
-		activeTab: 0,
-		items: [createBsicInfoForm(), createDetailGrid()]
-	});
-	tabPanel.on({
-		tabchange: (panel, newCard, oldCard) => {
-			console.log(newCard, oldCard);
-			if (newCard.id === 'detailGrid') {
-				const id = Ext.getCmp('qryForm').getForm().findField('squareStickId').getValue();
-				if (!id) {
-					return;
-				}
-				getJjDetailInfo(id, (data) => {
-					console.log(data);
-					const detailStore = Ext.data.StoreManager.lookup('detailStore');
-					detailStore.loadData(data);
-				});
-			}
-		}
-	});
-	return tabPanel;
+  const tabPanel = Ext.create("Ext.tab.Panel", {
+    region: "center",
+    id: "detailTabPanel",
+    activeTab: 0,
+    items: [createBsicInfoForm(), createDetailGrid()]
+  })
+  tabPanel.on({
+    tabchange: (panel, newCard, oldCard) => {
+      console.log(newCard, oldCard)
+      const id = Ext.getCmp("qryForm").getForm().findField("squareStickId").getValue()
+      if (!id) {
+        return
+      }
+      if (newCard.id === "detailGrid") {
+        const detailStore = Ext.data.StoreManager.lookup("detailStore")
+        detailStoreReload(detailStore)
+      } else if (newCard.id === "basicInfoForm") {
+        getJjBasicInfo(id, (data) => {
+          const basicForm = Ext.getCmp("basicInfoForm").getForm()
+          setBasicFormInfo(basicForm, data)
+        })
+      }
+    }
+  })
+  return tabPanel
 }
 
 function createBsicInfoForm() {
-	const basicInfoForm = Ext.create('InitViewer.BasicInfoForm', {
-		id: 'basicInfoForm',
-		title: '基本信息',
-		region: 'center'
-	});
-	return basicInfoForm;
+  const basicInfoForm = Ext.create("InitViewer.BasicInfoForm", {
+    id: "basicInfoForm",
+    title: "基本信息",
+    region: "center"
+  })
+  return basicInfoForm
 }
 
 function createPagingTool(store) {
-	const detailGrid = Ext.create('mycim.pagingtoolbar', {
-		id: 'detailPagingTool',
-		store: store,
-		dock: 'bottom'
-	});
+  const detailPagingTool = Ext.create("MyCim.PagingToolbar", {
+    id: "detailPagingTool",
+    store: store,
+    dock: "bottom",
+    displayInfo: true,
+    inputItemWidth: 50,
+    emptyMsg: i18n.msgs.MSG_NO_RESULT,
+    displayMsg: i18n_fld_showThe + "{0}" + i18n_fld_to + "{1}," + i18n_fld_total + "{2}" + i18n_fld_tiao
+  })
+  return detailPagingTool
 }
 
 function createDetailGrid() {
-	const detailStore = createDetailStore();
-	const detailGrid = Ext.create('InitViewer.DetailGrid', {
-		id: 'detailGrid',
-		store: detailStore,
-		title: '完工明细',
-		region: 'center'
-	});
-	return detailGrid;
+  const detailStore = createDetailStore()
+  const detailGrid = Ext.create("InitViewer.DetailGrid", {
+    id: "detailGrid",
+    store: detailStore,
+    title: "完工明细",
+    region: "center",
+    dockedItems: [createPagingTool(detailStore)]
+  })
+  return detailGrid
 }
 
 function createDetailStore() {
-	const detailStore = Ext.create('InitViewer.DetailStore', {
-		storeId: 'detailStore'
-	});
+  const detailStore = Ext.create("InitViewer.DetailStore", {
+    storeId: "detailStore",
+    pageSize: PAGE_SIZE
+  })
+  detailStore.setProxy({
+    type: "ajax",
+    url: "http://127.0.0.1:8080/ext/queryDetail",
+    params: {
+      start: 0,
+      limit: PAGE_SIZE
+    },
+    reader: {
+      root: "data",
+      totalProperty: "total"
+    }
+  })
+  return detailStore
+}
 
-	return detailStore;
+function detailStoreReload(store, exparam) {
+  store.loadPage(1, {
+    params: {
+      start: 0,
+      limit: PAGE_SIZE
+    }
+  })
+  if (exparam) {
+    store.proxy.extraParams = exparam
+  }
+}
+
+function setBasicFormInfo(basicForm, data) {
+  basicForm.findField("squareStickId").setValue(data.squareStickId)
+  basicForm.findField("workorderId").setValue(data.workorderId)
+  basicForm.findField("jjTechnologyWorkflowId").setValue(data.jjTechnologyWorkflowId)
+  basicForm.findField("technologyWorkflowVersion").setValue(data.technologyWorkflowVersion)
+  basicForm.findField("productSpecBook").setValue(data.productSpecBook)
+  basicForm.findField("squareStickStatus").setValue(data.squareStickStatus)
+  basicForm.findField("hairStickId").setValue(data.hairStickId)
+  basicForm.findField("hairStickFurnaceId").setValue(data.hairStickFurnaceId)
+  basicForm.findField("furnaceTaskId").setValue(data.furnaceTaskId)
 }
 
 function getJjDetailInfo(id, sucCallFn, failCallFn) {
-	$.ajax({
-		type: 'GET',
-		url: '../../data/jjDetials.json',
-		success: function (data) {
-			sucCallFn(data.data);
-		},
-		error: function (result) {}
-	});
+  $.ajax({
+    type: "GET",
+    url: "../../data/jjDetials.json",
+    success: function (data) {
+      sucCallFn(data.data)
+    },
+    error: function (result) {}
+  })
 }
 
 function getJjBasicInfo(id, sucCallFn, failCallFn) {
-	$.ajax({
-		type: 'GET',
-		url: '../../data/jjBasicInfo.json',
-		success: function (data) {
-			sucCallFn(data.data);
-		},
-		error: function (result) {}
-	});
+  $.ajax({
+    type: "GET",
+    url: "../../data/jjBasicInfo.json",
+    success: function (data) {
+      sucCallFn(data.data)
+    },
+    error: function (result) {}
+  })
 }
